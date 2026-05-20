baa9181762eddbec34126f684e9e30302a59551e
Mandeep <mandeep.singh.build@gmail.com>
2026-05-20 21:58:35 +0530

    Add an example code for if, else, and elif conditions in Python

---
+++ 1. if_elif_else_conditions/if_elif_else.py
@@ -0,0 +1,23 @@
+age = 25
+if age >= 18:
+    print("You can enter the club.")           # if
+
+age = 15
+if age >= 18:
+    print("You can enter the club.")
+else:                                          # else
+    print("Sorry, you are too young.")    
+
+temperature = 20
+if temperature >= 25 :
+    print("It is hot")                         # if / else
+else:
+    print("It is cold")
+
+total_score = 75
+if total_score >= 90:
+    print("Grade A")                          # if / elif / else
+elif total_score >= 70:
+    print("Grade B")
+else:
+    print("Grade C")        
