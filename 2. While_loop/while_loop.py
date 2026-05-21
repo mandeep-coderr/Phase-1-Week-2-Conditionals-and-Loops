f21ba5d81b9a9b2ce1b5d10d3bb4a9ab0db01609
Mandeep <mandeep.singh.build@gmail.com>
2026-05-21 22:10:02 +0530

    Add examples of while loops and loop variables in Python, demonstrating item packing, countdown timers, and search termination using break statements.

---
+++ 2. While_loop/while_loop.py
@@ -0,0 +1,38 @@
+# pushups = 0
+
+# while pushups < 3:
+#     print("Doing a pushup!")                                  #
+#     pushups = pushups + 1
+
+# TOPIC 1: The Loop Variable (The Box)
+items_packed = 0  
+
+# TOPIC 2: The While Loop (The Brain checking the rule)
+while items_packed < 3:  
+    print("Packing an item into the box...")
+    
+    # Updating the Loop Variable (The Data Flow)
+    items_packed = items_packed + 1  
+
+print("All items packed! Ready for delivery.")    
+
+#combined loop variable + while loop
+rocket_timer = 10
+
+while rocket_timer > 0:
+    print(rocket_timer)
+    rocket_timer = rocket_timer - 1
+
+print("Rocket_launched")
+
+#break
+search_number = 1
+
+while search_number <= 5:
+    if search_number == 4:
+        break
+        
+    print(search_number)
+    search_number = search_number + 1
+
+print("Search over.")
