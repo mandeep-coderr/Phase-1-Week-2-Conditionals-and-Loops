a760a7e6058eed2e0173b297b9822b7d2f945c49
Mandeep <mandeep.singh.build@gmail.com>
2026-05-23 22:10:41 +0530

    Add nested loop examples in Python, demonstrating various scenarios including counting actions, simulating pizza orders, and handling break statements within loops for enhanced understanding of loop behavior.

---
+++ 4. Nested Loops/nested_loops.py
@@ -0,0 +1,39 @@
+for sets in range(1 , 4):   
+    for pushups in range(1 , 6):       
+        print(f"set is {sets} pushup is {pushups}") 
+
+#outer loop runs once - inner loop runs completely
+for order in range(1 , 3):
+    print(f"Starting Order{order}")
+
+    for pizza in range(1 , 4):
+        print(f"Baking Pizza{pizza}")
+
+    print(f"Order{order} is out for delivery") 
+
+#Time complexity intuition nested loop = n ** 2 iteration for n items - n stand for number of items
+
+total_actions = 0
+items = 10
+
+for outer in range(items):
+    for inner in range(items):
+        total_actions = total_actions + 1
+
+print(f"For {items} items, the computer did {total_actions} actions.")
+
+# Break inside nested loop
+for floor in range(1, 4):
+    print(f"Arrived at Floor {floor}")
+    
+    for room in range(1, 5):
+        if room == 3:
+            print("  TIGER IN ROOM 3! BREAKING THE LOOP!")
+            break
+            
+        print(f"  Checked Room {room} - All safe")
+
+
+
+
+        
