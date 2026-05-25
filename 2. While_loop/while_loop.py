00dbbe8c72ff8f1174ef3f22625a67682b80eedb
Mandeep <mandeep.singh.build@gmail.com>
2026-05-25 23:24:55 +0530

    Refactor while loop example by commenting out functional code for instructional purposes; improved organization and clarity of loop concepts.

--- 2. While_loop/while_loop.py
+++ 2. While_loop/while_loop.py
@@ -1,28 +1,28 @@
-pushups = 0
+# pushups = 0
 
-while pushups < 3:
-    print("Doing a pushup!")                                  #
-    pushups = pushups + 1
+# while pushups < 3:
+#     print("Doing a pushup!")                                  #
+#     pushups = pushups + 1
 
-# TOPIC 1: The Loop Variable (The Box)
-items_packed = 0  
+# # TOPIC 1: The Loop Variable (The Box)
+# items_packed = 0  
 
- # TOPIC 2: The While Loop 
-while items_packed < 3:  
-    print("Packing an item into the box...")
+#  # TOPIC 2: The While Loop 
+# while items_packed < 3:  
+#     print("Packing an item into the box...")
     
-    items_packed = items_packed + 1  
+#     items_packed = items_packed + 1  
 
-print("All items packed! Ready for delivery.")    
+# print("All items packed! Ready for delivery.")    
 
-#combined loop variable + while loop
-rocket_timer = 10
+# #combined loop variable + while loop
+# rocket_timer = 10
 
-while rocket_timer > 0:
-    print(rocket_timer)
-    rocket_timer = rocket_timer - 1
+# while rocket_timer > 0:
+#     print(rocket_timer)
+#     rocket_timer = rocket_timer - 1
 
-print("Rocket_launched")
+# print("Rocket_launched")
 
 #break
 search_number = 1
